6767b7ef4ee11e63a7665aa9400f20a71045a9d2
HarmvZ <harm.van.zeeland@gmail.com>
2018-02-02 08:33:32 +0100

    Enable Ropsten test network connection

--- truffle.js
+++ truffle.js
@@ -1,3 +1,8 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+var infura_apikey = "6hfKKe2tZUTHMCY5YTBT";
+var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -7,6 +12,11 @@ module.exports = {
       host: "192.168.100.125",
       port: 9545,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"+infura_apikey),
+      network_id: 3,
+      gas: 4600000
     }
   }
 };
